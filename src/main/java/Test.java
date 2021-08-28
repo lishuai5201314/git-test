2c2f9846c0dcd3b7c04e351306174babb0d92b5e
lishuai <1040212303@qq.com>
2021-08-28 21:52:03 +0800

    hot-fix test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,5 +2,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("hhh");
         System.out.println("banben");
+        System.out.println("hot-fix");
     }
 }
